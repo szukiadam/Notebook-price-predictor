55c7cfe5660f98418bd3dc79a77706922c1e45ae
szukiadam <61733719+szukiadam@users.noreply.github.com>
2021-05-22 16:14:50 +0200

    type in Flash -> Flask

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flash import Flask, request, jsonify, render_template
 import pickle
 
-app = Flash(__name__)
+app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')

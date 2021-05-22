4f063a8c761eca8c3f50e6d994e7991f7c3d0f02
szukiadam <61733719+szukiadam@users.noreply.github.com>
2021-05-22 15:38:58 +0200

    added files for deployment

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flash import Flask, request, jsonify, render_template
+import pickle
+
+app = Flash(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home:
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text=f'Notebook Price should be: {output} Ft')
+
+if __name__ == '__main__':
+    app.run(debug=True)
